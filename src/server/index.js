1260b058ca15bc56d9cb7b761a7d890a126df353
timpei <timtimpei@hotmail.com>
2015-05-09 01:31:18 -0400

    moved models into src dir

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ import config from './appconfig';
 import express from 'express';
 import morgan from 'morgan';
 import routes from './routes';
-import models from "../../models";
+import models from "./models";
 
 const app = express();
 app.use(morgan('combined'));
--- src/server/models/index.js
+++ src/server/models/index.js
@@ -5,7 +5,6 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
 var sequelize = new Sequelize('trail', null, null, {
                       "dialect": "sqlite",
                       "storage": "./db.development.sqlite"
