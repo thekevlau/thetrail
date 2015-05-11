23dcd4eae50f986d5d4fc0ff58a391c2b7290879
Shahmeer Navid <shahmeernavid@gmail.com>
2015-05-10 21:29:17 -0400

    Tasks.

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,7 +7,13 @@ require('./tasks/server');
 require('./tasks/shared');
 require('./tasks/client');
 
-gulp.task('start', ['watch:server', 'watch:shared', 'watch:client'], function(){
+gulp.task('move:static_assets', function(){
+    gulp.src('./static/**/*')
+        .pipe(gulp.dest('./dist/client/'));
+});
+
+gulp.task('start', ['', 'watch:server', 'watch:shared', 'watch:client', 'move:static_assets'],
+        function(){
     nodemon({
         execMap: { js: 'node' },
         script: './dist/server/index.js',
